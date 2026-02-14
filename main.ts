7a3c1915f91513032ef43a35fdb46395936d5ee4
MINTspark <info@mintspark.io>
2026-02-14 23:24:01 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
+let radioGroup = 123;
+radio.setGroup(radioGroup);
 
+input.onButtonPressed(Button.A, function() {
+    radio.sendString("chassis speed x 0.4 y 0.4 z 0;")
+})
+
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("chassis speed x 0 y 0 z 0;")
+})
